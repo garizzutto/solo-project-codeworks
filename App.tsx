a90a0af0fb54372d50633d5bcaaba520104e7294
Gabriel Almeida Rizzutto <garizzutto@gmail.com>
2022-11-10 23:25:25 -0300

    feat: husky added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 'use-strict';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
